b422f17f45b2cbff02bd873d10e01cb1f50bd8ed
Kate Lee <isoeun809@gmail.com>
2022-07-28 13:58:15 -0500

    Initial Commit

---
+++ Google Search/Google_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_SearchApp.swift
+//  Google Search
+//
+//  Created by 이소은 on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct Google_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
