c692171d52bbed4e3e6719bad2a6ce48f4afe44a
Kate Lee <isoeun809@gmail.com>
2022-07-29 14:32:54 -0500

    Setup Contentview

--- Google Search/ContentView.swift
+++ Google Search/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var entries = [Entry]()
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(entries) { entry in
+                Link(destination: URL(string: entry.link)!, label: {
+                    Text(entry.title)
+                })
+            }
+            .navigationTitle("Corn Dogs")
+        }
+        .onAppear(perform: {
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
+        })
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+struct Entry: Identifiable {
+    let id: UUID
+    let title: String
+    let link: String
+}
+
+
+
+
