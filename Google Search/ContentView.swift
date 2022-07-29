6d741733fc8a703eaf6157dd782e6db819910451
Kate Lee <isoeun809@gmail.com>
2022-07-29 14:46:17 -0500

    Finished setting up alert

--- Google Search/ContentView.swift
+++ Google Search/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
             .navigationTitle("Corn Dogs")
         }
         .onAppear(perform: {
+            queryAPI()
         })
         .alert(isPresented: $showingAlert, content: {
             Alert(title: Text("Loading Error"),
@@ -28,6 +29,25 @@ struct ContentView: View {
                   dismissButton: .default(Text("OK")))
         })
     }
+    func queryAPI() {
+        let apiKey = "?rapidapi-key = 758f477fdcmsh2b78f5fdd0bf86bp1ccc3fjsnbd45cb1f4332"
+        let query = "https://google-search3.p.rapidapi.com/api/v1/search/q=corn+dog&num=100?rapidapi-key="
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                let contents = json["results"].arrayValue
+                for item in contents {
+                    let title = item["title"].stringValue
+                    let link = item["link"].stringValue
+                    let entry = Entry(title: title, link: link)
+                    entries.append(entry)
+                }
+                return
+            }
+        }
+        showingAlert = true
+    }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
@@ -37,7 +57,7 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct Entry: Identifiable {
-    let id: UUID
+    let id = UUID()
     let title: String
     let link: String
 }
